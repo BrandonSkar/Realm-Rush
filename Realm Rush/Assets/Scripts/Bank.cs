439953f4aaab2a5455f418b421a4529ad826e7e9
BrandonSkar <bransk01@hotmail.com>
2021-12-10 20:28:58 -0800

    Created a UI
    Gold is now displayed and updates when enemies get to the end or when towers are placed or when enemies are killed

--- Realm Rush/Assets/Scripts/Bank.cs
+++ Realm Rush/Assets/Scripts/Bank.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -11,24 +12,34 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateGoldDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateGoldDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateGoldDisplay();
 
-        if(currentBalance < 0)
+        if (currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateGoldDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     private void ReloadScene()
